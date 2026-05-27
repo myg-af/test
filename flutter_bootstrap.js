84b74835a8e61d94835c93dbded5be7f9a03890d
myg-af <mygnyh@gmail.com>
2026-05-27 07:52:52 +0200

    deploy: loading bar 50% real + 50% fake animation

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,7 +38,7 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1002089986"
+    serviceWorkerVersion: "132634260"
   },
   config: { hostElement: document.querySelector('#app') }
 });
