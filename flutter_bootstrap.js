fb61825aeda97da8020e3e46ce70b4e572f29a67
myg-af <mygnyh@gmail.com>
2026-05-27 06:48:56 +0200

    deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,7 +38,7 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3808089151"
+    serviceWorkerVersion: "825843376"
   },
   config: { hostElement: document.querySelector('#app') }
 });
